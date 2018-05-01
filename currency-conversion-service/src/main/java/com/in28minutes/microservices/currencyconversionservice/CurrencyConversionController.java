e7882940ed19d6979c47bada6db5b3c557a00d30
Mark Sage <msage@gogoair.com>
2018-04-30 19:43:40 -0500

    refactor

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -35,7 +35,7 @@ public class CurrencyConversionController {
 	}
 
 	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
-	public CurrencyConversionBean convertCurrencyFeigh(@PathVariable String from,
+	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from,
 												  @PathVariable String to,
 												  @PathVariable BigDecimal quantity){
 		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
