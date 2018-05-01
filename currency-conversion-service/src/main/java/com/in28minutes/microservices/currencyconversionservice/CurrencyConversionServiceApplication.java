ac11d1d77f73552f61e72d5736f46e0f8d8469d3
Mark Sage <msage@gogoair.com>
2018-04-30 21:53:57 -0500

    eureka client

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,14 +2,12 @@ package com.in28minutes.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @SpringBootApplication
 @EnableFeignClients
-@ComponentScan("com.in28minutes.microservices")
+@EnableEurekaClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
