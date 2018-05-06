fc4d9b9de8e1fcf8d984eb35857405d43aaf0c88
Mark Sage <msage@gogoair.com>
2018-05-06 13:39:33 -0500

    Hystrix

--- limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,11 +1,16 @@
 package com.in28minutes.microservices.limitsservice;
 
 import com.in28minutes.microservices.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	private Configuration configuration;
 
@@ -16,5 +21,15 @@ public class LimitsConfigurationController {
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfiguration(){
 		return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
+	}
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+	public LimitConfiguration retrieveConfiguration(){
+		throw new RuntimeException("ouch");
+	}
+
+	public LimitConfiguration fallbackRetrieveConfiguration(Throwable t){
+		logger.error("In fallback",t);
+		return new LimitConfiguration(999, 9);
 	}
 }
